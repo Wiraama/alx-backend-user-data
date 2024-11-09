f9c5f9dd3ccfad754d94dd8a4b415c2c354596e3
Wiraama <mawira01@students.uonbi.ac.ke>
2024-11-09 16:04:11 +0300

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
+""" modure tu return logging """
 import re
 from typing import List
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    return re.sub(rf'({"|".join(fields)})=[^{separator}]+', lambda m: f"{m.group().split('=')[0]}={redaction}", message)
+
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
+    """ function """
+    return re.sub(
+            rf'({"|".join(fields)})=[^{separator}]+',
+            lambda m: f"{m.group().split('=')[0]}={redaction}", message)
