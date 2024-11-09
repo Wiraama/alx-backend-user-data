3a0fc8b8f1da7f56ca6506bd8cc85e3fdc2becbf
Wiraama <mawira01@students.uonbi.ac.ke>
2024-11-09 15:54:43 +0300

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 import re
-def filter_datum(fields, redaction, message, separator):
+from typing import List
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     return re.sub(rf'({"|".join(fields)})=[^{separator}]+', lambda m: f"{m.group().split('=')[0]}={redaction}", message)
