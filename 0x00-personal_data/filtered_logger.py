07790138ef01b11f91e31b5012e4397ada6a7990
Wiriama <151209743+Wiraama@users.noreply.github.com>
2024-11-10 12:36:19 +0300

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -27,11 +27,9 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-
     def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields: List[str] = fields
-
 
     def format(self, record: logging.LogRecord) -> str:
         """ formatting logs records """
