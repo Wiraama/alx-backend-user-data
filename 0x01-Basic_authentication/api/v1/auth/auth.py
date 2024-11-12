380e6a423d8212d63acd26ab0a60a65878803901
Wiraama <mawira01@students.uonbi.ac.ke>
2024-11-12 17:09:36 +0300

    auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+from flask import request, Flask
+from typing import List, TypeVar
+
+
+class Auth:
+    """ class Auth """
+
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ returns False path"""
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """ return None fopr now """
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ return none for now """
+        return None
