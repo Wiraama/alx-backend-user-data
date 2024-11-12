fce99072207863d9e3d3e01ddce0528c6b687c17
Wiraama <mawira01@students.uonbi.ac.ke>
2024-11-12 17:30:48 +0300

    auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,16 +7,13 @@ from typing import List, TypeVar
 class Auth:
     """ class Auth """
 
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ returns False path"""
         return False
 
-
     def authorization_header(self, request=None) -> str:
         """ return None fopr now """
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ return none for now """
