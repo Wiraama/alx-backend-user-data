fccac78f0dd9907bb3572f09b192033b489bd449
Wiraama <mawira01@students.uonbi.ac.ke>
2024-11-12 17:54:44 +0300

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,8 +8,17 @@ class Auth:
     """ class Auth """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ returns False path"""
-        return False
+        """
+        args: path, excluded_path
+        return: boolean"""
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        path = path if path.endswith('/') else path + '/'
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ return None fopr now """
