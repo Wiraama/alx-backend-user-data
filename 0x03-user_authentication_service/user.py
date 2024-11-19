09da6270e0dd4c69062346e455cf25e0163a6053
Wiraama <mawira01@students.uonbi.ac.ke>
2024-11-19 22:51:48 +0300

    user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
 """ user model """
-from flask import Flask
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
 
 """ initializing """
-app = Flask(__name__)
 Base = declarative_base()
-DATABASE_URI = 'sqlite:///users.db'
+
 
 class User(Base):
     """ user class of storange """
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
