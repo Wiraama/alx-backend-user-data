6559f22a2dba2b52f34c88772c76b57187d487ef
Wiraama <mawira01@students.uonbi.ac.ke>
2024-11-20 11:27:34 +0300

    db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,8 +31,8 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """ add user to session and return user
         """
-         new_user = User(email=email, hashed_password=hashed_password)
-         try:
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
             self._session.add(new_user)
             self._session.commit()
         except Exception as e:
