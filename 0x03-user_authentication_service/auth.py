ea63064e40ef808a0a93871424f8c9503ba0d0d0
Wiraama <mawira01@students.uonbi.ac.ke>
2024-11-21 22:06:20 +0300

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -30,7 +30,7 @@ class Auth:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError(f"{email} already exists")
 
-    def valid_login(self, email, password) -> bool:
+    def valid_login(self, email: str, password: str) -> bool:
         """ this function validate login
         """
         user = None
