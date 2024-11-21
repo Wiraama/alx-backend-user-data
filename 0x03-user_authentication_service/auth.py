0ff2371c104bdad2de367c94dc9b007f66b8464c
Wiraama <mawira01@students.uonbi.ac.ke>
2024-11-22 00:32:50 +0300

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -15,6 +15,12 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
+def _generate_auth() -> str:
+    """ generate uuid
+    """
+    return str(uuid.uuid4())
+
+
 class Auth:
     """
     Auth class to interact with the authentication database
@@ -46,7 +52,3 @@ class Auth:
             return False
         return False
 
-    def _generate_uuid() -> str:
-        """ generated uuid
-        """
-        return str(uuid.uuid4())
