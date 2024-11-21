224a1d7a216b3c2693851066b95585c8ebde753d
Wiraama <mawira01@students.uonbi.ac.ke>
2024-11-21 16:11:54 +0300

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -24,7 +24,7 @@ def users() -> str:
 
     try:
         AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"}), 201
+        return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
